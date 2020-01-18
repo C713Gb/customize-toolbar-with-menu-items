5638fa88f969d894c3eb1551644715150cf5a339
GouravBanerjee <banerjeec713@gmail.com>
2020-01-18 23:11:31 +0530

    Custom Toolbar with menu items

---
+++ app/src/main/java/com/example/customtoolbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.customtoolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.topright_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.user_profile:
+                Toast.makeText(this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.send:
+                Toast.makeText(this, "Send Selected", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
